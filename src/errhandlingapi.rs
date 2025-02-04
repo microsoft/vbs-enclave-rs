c6bd13dede39a6b4a4f5c4d19441b6be3b9aeb65
Joe Ranweiler <joranwei@microsoft.com>
2025-02-04 12:38:32 -0800

    Import `GetLastError`

--- src/errhandlingapi.rs
+++ src/errhandlingapi.rs
@@ -1,7 +1,5 @@
-extern "system" {
-    fn GetLastError() -> u32;
-}
-
 pub fn get_last_error() -> u32 {
-    unsafe { GetLastError() }
+    unsafe {
+        windows_sys::Win32::Foundation::GetLastError()
+    }
 }
