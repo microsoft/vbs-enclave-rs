344bea2a831ede78b2751e7be1d93a5a9780bd0f
Josh Watson <josh@joshwatson.com>
2025-02-25 11:33:08 -0800

    Refactor SampleHost

--- sample/SampleHost/Main.cpp
+++ sample/SampleHost/Main.cpp
@@ -2,6 +2,9 @@
 #include <cstdint>
 #include <iostream>
 #include <string>
+#include <memory>
+#include <utility>
+#include <wil/resource.h>
 
 // These suppress some warnings for casting LPVOID to HRESULT
 // and other truncation type issues when passing parameters and
@@ -9,6 +12,13 @@
 #pragma warning (disable : 4311)
 #pragma warning (disable : 4302)
 #pragma warning (disable : 4267)
+
+void CleanupEnclave(void* enclave_base) {
+	TerminateEnclave(enclave_base, TRUE);
+	DeleteEnclave(enclave_base);
+}
+
+using Enclave = std::unique_ptr<void, decltype(&CleanupEnclave)>;
 
 const uint8_t OwnerId[IMAGE_ENCLAVE_LONG_ID_LENGTH] = {0x10, 0x20, 0x30, 0x40, 0x41, 0x31, 0x21, 0x11};
 
@@ -37,6 +47,8 @@ struct DecryptDataParams
 	uint8_t *decrypted_data;
 };
 
+const uint32_t KeySize = 256;
+
 struct AES256KeyBlob
 {
 	BCRYPT_KEY_DATA_BLOB_HEADER header;
@@ -45,7 +57,8 @@ struct AES256KeyBlob
 
 uint8_t *AllocateCallback(size_t size)
 {
-	return new uint8_t[size];
+	auto allocation = std::make_unique<uint8_t[]>(size);
+	return allocation.release();
 }
 
 char hexchars[] = "0123456789abcdef";
@@ -78,17 +91,7 @@ void DumpBytes(_In_ uint8_t* buffer, size_t allocation_size) {
 
 }
 
-int wmain(int argc, wchar_t **argv)
-{
-	if (argc != 3)
-	{
-		std::cerr << "Usage: EnclaveLoader.exe <enclave DLL path> <message>" << std::endl;
-		return -1;
-	}
-
-	std::wstring enclave_name = argv[1];
-	std::wstring message = argv[2];
-
+void* StartEnclave(std::wstring enclave_name) {
 	ENCLAVE_CREATE_INFO_VBS info{};
 
 #ifdef _DEBUG
@@ -97,7 +100,7 @@ int wmain(int argc, wchar_t **argv)
 
 	CopyMemory(&info.OwnerID, OwnerId, IMAGE_ENCLAVE_LONG_ID_LENGTH);
 
-	LPVOID enclave_base = CreateEnclave(
+	void* enclave_base = CreateEnclave(
 		GetCurrentProcess(),
 		nullptr,
 		0x10000000,
@@ -110,13 +113,13 @@ int wmain(int argc, wchar_t **argv)
 	if (enclave_base == nullptr)
 	{
 		std::cerr << "CreateEnclave failed: " << GetLastError() << std::endl;
-		return -1;
+		return nullptr;
 	}
 
 	if (!LoadEnclaveImage(enclave_base, enclave_name.c_str()))
 	{
 		std::cerr << "LoadEnclaveImage failed: " << GetLastError() << std::endl;
-		return -1;
+		return nullptr;
 	}
 
 	ENCLAVE_INIT_INFO_VBS init_info{};
@@ -126,20 +129,14 @@ int wmain(int argc, wchar_t **argv)
 	if (!InitializeEnclave(GetCurrentProcess(), enclave_base, &init_info, sizeof(init_info), nullptr))
 	{
 		std::cerr << "InitializeEnclave failed: " << GetLastError() << std::endl;
-		return -1;
+		return nullptr;
 	}
 
-	std::cout << "Rust enclave created and initialized!" << std::endl;
+	return enclave_base;
+}
 
-	PENCLAVE_ROUTINE new_keypair = (PENCLAVE_ROUTINE)GetProcAddress((HMODULE)enclave_base, "new_keypair");
-	PENCLAVE_ROUTINE generate_report = (PENCLAVE_ROUTINE)GetProcAddress((HMODULE)enclave_base, "generate_report");
-	PENCLAVE_ROUTINE decrypt_data = (PENCLAVE_ROUTINE)GetProcAddress((HMODULE)enclave_base, "decrypt_data");
-
-	std::cout << "new_keypair is " << std::hex << new_keypair << std::endl;
-	std::cout << "generate_report is " << std::hex << generate_report << std::endl;
-	std::cout << "decrypt_data is " << std::hex << decrypt_data << std::endl;
-
-	BCRYPT_KEY_HANDLE keypair = nullptr;
+wil::unique_bcrypt_key GenerateECDHKeypair() {
+	wil::unique_bcrypt_key keypair = nullptr;
 
 	NTSTATUS status = BCryptGenerateKeyPair(
 		BCRYPT_ECDH_P256_ALG_HANDLE,
@@ -150,19 +147,23 @@ int wmain(int argc, wchar_t **argv)
 
 	if (status != 0) {
 		std::cerr << "BCryptGenerateKeyPair failed: " << std::hex << status << std::endl;
-		return status;
+		return nullptr;
 	}
 
-	status = BCryptFinalizeKeyPair(keypair, 0);
+	status = BCryptFinalizeKeyPair(keypair.get(), 0);
 
 	if (status != 0) {
 		std::cerr << "BCryptFinalizeKeyPair failed: " << std::hex << status << std::endl;
-		return status;
+		return nullptr;
 	}
 
+	return keypair;
+}
+
+std::unique_ptr<uint8_t[]> ExportPublicKey(BCRYPT_KEY_HANDLE keypair) {
 	uint32_t blob_size = 0;
 
-	status = BCryptExportKey(
+	NTSTATUS status = BCryptExportKey(
 		keypair,
 		nullptr,
 		BCRYPT_ECCPUBLIC_BLOB,
@@ -174,21 +175,26 @@ int wmain(int argc, wchar_t **argv)
 
 	if (status != 0) {
 		std::cerr << "BCryptExportKey failed: " << std::hex << status << std::endl;
-		return status;
+		return nullptr;
 	}
 
 	if (blob_size != sizeof(BCRYPT_ECCKEY_BLOB) + ENCLAVE_REPORT_DATA_LENGTH) {
 		std::cerr << "Export blob size is " << std::dec << blob_size << " bytes, expected " << sizeof(BCRYPT_ECCKEY_BLOB) + ENCLAVE_REPORT_DATA_LENGTH << " bytes!" << std::endl;
-		return -1;
+		return nullptr;
 	}
 
-	uint8_t* my_public_key_blob = new uint8_t[blob_size];
+	auto my_public_key_blob = std::make_unique<uint8_t[]>(blob_size);
+
+	if (nullptr == my_public_key_blob) {
+		std::cerr << "Failed to allocate " << std::dec << blob_size << " bytes for public key!" << std::endl;
+		return nullptr;
+	}
 
 	status = BCryptExportKey(
 		keypair,
 		nullptr,
 		BCRYPT_ECCPUBLIC_BLOB,
-		my_public_key_blob,
+		my_public_key_blob.get(),
 		blob_size,
 		(ULONG*)&blob_size,
 		0
@@ -196,52 +202,59 @@ int wmain(int argc, wchar_t **argv)
 
 	if (status != 0) {
 		std::cerr << "Second BCryptExportKey failed: " << std::hex << status << std::endl;
-		return status;
+		return nullptr;
 	}
 
-	std::cout << "Creating new enclave key and providing host public key..." << std::endl;
+	return my_public_key_blob;
+}
 
-	NewKeypairParams new_keypair_params = NewKeypairParams{256, my_public_key_blob};
-	LPVOID result = nullptr;
+bool CreateEnclaveKeypair(void* enclave_base, uint8_t* public_key_blob) {
+	PENCLAVE_ROUTINE new_keypair = (PENCLAVE_ROUTINE)GetProcAddress((HMODULE)enclave_base, "new_keypair");
+
+	if (nullptr == new_keypair) {
+		std::cerr << "Failed to get new_keypair entry point! Error: " << std::hex << HRESULT_FROM_WIN32(GetLastError()) << std::endl;
+		return false;
+	}
+
+	NewKeypairParams new_keypair_params = NewKeypairParams{ KeySize, public_key_blob };
+	void* result = nullptr;
 
 	if (!CallEnclave(new_keypair, &new_keypair_params, TRUE, &result) || (HRESULT)result != S_OK)
 	{
 		std::cerr << "new_keypair failed: " << std::hex << (HRESULT)result << std::endl;
-		return (HRESULT)result;
+		return false;
 	}
 
-	std::cout << "New keypair created!" << std::endl;
+	return true;
+}
 
-	GenerateReportParams generate_report_params = GenerateReportParams{AllocateCallback};
+std::unique_ptr<uint8_t[]> GetAttestationReport(void* enclave_base) {
+	PENCLAVE_ROUTINE generate_report = (PENCLAVE_ROUTINE)GetProcAddress((HMODULE)enclave_base, "generate_report");
+
+	if (nullptr == generate_report) {
+		std::cerr << "Failed to get generate_report entry point! Error: " << std::hex << HRESULT_FROM_WIN32(GetLastError()) << std::endl;
+		return nullptr;
+	}
+
+	GenerateReportParams generate_report_params = GenerateReportParams{ AllocateCallback };
+	void* result = nullptr;
 
 	if (!CallEnclave(generate_report, &generate_report_params, TRUE, &result) || (HRESULT)result != S_OK)
 	{
 		std::cerr << "generate_report failed: " << std::hex << (HRESULT)result << std::endl;
-		return (HRESULT)result;
+		return nullptr;
 	}
 
 	std::cout << "Report generated! " << std::dec << generate_report_params.report_size << " bytes!" << std::endl;
 
-	VBS_ENCLAVE_REPORT *report = (VBS_ENCLAVE_REPORT *)((VBS_ENCLAVE_REPORT_PKG_HEADER *)generate_report_params.report + 1);
+	auto report_buffer = std::unique_ptr<uint8_t[]>(generate_report_params.report);
+	return report_buffer;
+}
 
-	/* This is where you would validate the attestation report before using the public key! */
-
-	std::cout << "Beep boop beep, validating attestation report... (for pretend)" << std::endl;
-
-	Sleep(1000);
-
-	std::cout << "Enclave is validated!" << std::endl;
-
-	BCRYPT_ECCKEY_BLOB *public_key_blob = (BCRYPT_ECCKEY_BLOB *)new uint8_t[sizeof(BCRYPT_ECCKEY_BLOB) + ENCLAVE_REPORT_DATA_LENGTH];
-
-	public_key_blob->dwMagic = BCRYPT_ECDH_PUBLIC_P256_MAGIC;
-	public_key_blob->cbKey = ENCLAVE_REPORT_DATA_LENGTH / 2;
-
-	memcpy(public_key_blob + 1, report->EnclaveData, ENCLAVE_REPORT_DATA_LENGTH);
-
+wil::unique_bcrypt_key DeriveSharedKey(BCRYPT_KEY_HANDLE keypair, uint8_t* public_key_blob) {
 	BCRYPT_KEY_HANDLE public_key_handle = nullptr;
 
-	status = BCryptImportKeyPair(
+	NTSTATUS status = BCryptImportKeyPair(
 		BCRYPT_ECDH_P256_ALG_HANDLE,
 		nullptr,
 		BCRYPT_ECCPUBLIC_BLOB,
@@ -254,18 +267,18 @@ int wmain(int argc, wchar_t **argv)
 	if (status != 0)
 	{
 		std::cerr << "BCryptImportKeyPair failed: " << std::hex << status << std::endl;
-		return status;
+		return nullptr;
 	}
 
 	std::cout << "Public key imported successfully!" << std::endl;
 
-	BCRYPT_KEY_HANDLE agreed_secret = nullptr;
+	wil::unique_bcrypt_key agreed_secret = nullptr;
 
 	status = BCryptSecretAgreement(keypair, public_key_handle, &agreed_secret, 0);
 
 	if (status != 0) {
 		std::cerr << "BCryptSecretAgreement failed: " << std::hex << status << std::endl;
-		return status;
+		return nullptr;
 	}
 
 	BCryptBuffer buffer = {
@@ -290,43 +303,41 @@ int wmain(int argc, wchar_t **argv)
 	};
 	uint32_t bytes_needed = 0;
 
-	status = BCryptDeriveKey(agreed_secret, BCRYPT_KDF_HASH, &parameter_list, derived_key_blob.key, sizeof(derived_key_blob.key), (ULONG*)&bytes_needed, 0);
+	status = BCryptDeriveKey(agreed_secret.get(), BCRYPT_KDF_HASH, &parameter_list, derived_key_blob.key, sizeof(derived_key_blob.key), (ULONG*)&bytes_needed, 0);
 
 	if (status != 0) {
 		std::cerr << "BCryptDeriveKey failed: " << std::hex << status << std::endl;
-		return status;
+		return nullptr;
 	}
 
-	BCRYPT_KEY_HANDLE ephemeral_key = nullptr;
+	wil::unique_bcrypt_key ephemeral_key = nullptr;
 
-	status = BCryptImportKey(BCRYPT_AES_GCM_ALG_HANDLE, nullptr, BCRYPT_KEY_DATA_BLOB, &ephemeral_key, nullptr, 0, (uint8_t*)& derived_key_blob, sizeof(derived_key_blob), 0);
+	status = BCryptImportKey(BCRYPT_AES_GCM_ALG_HANDLE, nullptr, BCRYPT_KEY_DATA_BLOB, &ephemeral_key, nullptr, 0, (uint8_t*)&derived_key_blob, sizeof(derived_key_blob), 0);
 
 	if (status != 0) {
 		std::cerr << "BCryptImportKey failed: " << std::hex << status << std::endl;
-		return status;
+		return nullptr;
 	}
 
-	std::cout << "Successfully derived shared ephemeral key!" << std::endl;
+	return ephemeral_key;
+}
 
-	std::cout << "Encrypting the message:" << std::endl;
+using BufferInfo = std::pair<uint8_t*, size_t>;
+BufferInfo EncryptMessage(BCRYPT_HANDLE ephemeral_key, std::wstring message, BCRYPT_AUTHENTICATED_CIPHER_MODE_INFO& mode_info) {
 	DumpBytes((uint8_t*)message.c_str(), message.size() * sizeof(wchar_t));
 	std::cout << std::endl;
 
 	uint8_t iv[12] = {};
-	uint8_t tag[16] = {};
-
-	BCRYPT_AUTHENTICATED_CIPHER_MODE_INFO mode_info{};
-	BCRYPT_INIT_AUTH_MODE_INFO(mode_info);
 
 	mode_info.pbNonce = iv;
 	mode_info.cbNonce = sizeof(iv);
-	mode_info.pbTag = tag;
-	mode_info.cbTag = sizeof(tag);
 
-	status = BCryptEncrypt(
+	uint32_t bytes_needed = 0;
+
+	NTSTATUS status = BCryptEncrypt(
 		ephemeral_key,
 		(PUCHAR)message.c_str(),
-		message.size() * sizeof(WCHAR),
+		message.size() * sizeof(wchar_t),
 		&mode_info,
 		nullptr,
 		0,
@@ -338,10 +349,15 @@ int wmain(int argc, wchar_t **argv)
 
 	if (status != 0) {
 		std::cerr << "BCryptEncrypt failed to get size: " << std::hex << status << std::endl;
-		return status;
+		return std::make_pair(nullptr, 0);
 	}
 
-	uint8_t *ciphertext = new uint8_t[bytes_needed];
+	auto ciphertext = new uint8_t[bytes_needed];
+
+	if (ciphertext == nullptr) {
+		std::cerr << "Failed to allocate buffer for ciphertext" << std::endl;
+		return std::make_pair(nullptr, 0);
+	}
 
 	status = BCryptEncrypt(
 		ephemeral_key,
@@ -358,25 +374,130 @@ int wmain(int argc, wchar_t **argv)
 
 	if (status != 0) {
 		std::cerr << "BCryptEncrypt failed to encrypt: " << std::hex << status << std::endl;
-		return status;
+		return std::make_pair(nullptr, 0);
 	}
-	
-	std::cout << "Message successfully encrypted! Sending to enclave to decrypt..." << std::endl;
 
-	DecryptDataParams decrypt_data_params = { AllocateCallback, bytes_needed, ciphertext, mode_info.cbTag, mode_info.pbTag };
+	return std::make_pair(ciphertext, bytes_needed);
+}
+
+std::wstring DecryptMessage(void* enclave_base, DecryptDataParams &decrypt_data_params) {
+	PENCLAVE_ROUTINE decrypt_data = (PENCLAVE_ROUTINE)GetProcAddress((HMODULE)enclave_base, "decrypt_data");
+
+	if (nullptr == decrypt_data) {
+		std::cerr << "Could not get at least one of the enclave routines!" << std::endl;
+		return nullptr;
+	}
+
+	void* result = nullptr;
 
 	if (!CallEnclave(decrypt_data, &decrypt_data_params, TRUE, &result) || (HRESULT)result != S_OK)
 	{
 		std::cerr << "decrypt_data failed: " << std::hex << (HRESULT)result << std::endl;
-		return (HRESULT)result;
+		return nullptr;
 	}
 
+	std::wstring decrypted_data((const wchar_t*)decrypt_data_params.decrypted_data, decrypt_data_params.decrypted_size / sizeof(wchar_t));
+
+	delete[] decrypt_data_params.decrypted_data;
+	decrypt_data_params.decrypted_data = nullptr;
+
+	return decrypted_data;
+}
+
+int wmain(int argc, wchar_t **argv)
+{
+	if (argc != 3)
+	{
+		std::cerr << "Usage: EnclaveLoader.exe <enclave DLL path> <message>" << std::endl;
+		return -1;
+	}
+
+	std::wstring message = argv[2];
+
+	Enclave enclave_base(StartEnclave(argv[1]), &CleanupEnclave);
+
+	if (nullptr == enclave_base) {
+		return -1;
+	}
+
+	std::cout << "Rust enclave created and initialized!" << std::endl;
+
+	std::cout << "Creating host keypair..." << std::endl;
+
+	auto keypair = GenerateECDHKeypair();
+
+	if (!keypair.is_valid()) {
+		return -1;
+	}
+
+	auto my_public_key_blob = ExportPublicKey(keypair.get());
+
+	std::cout << "Creating new enclave key and providing host public key..." << std::endl;
+
+	if (!CreateEnclaveKeypair(enclave_base.get(), my_public_key_blob.get())) {
+		return -1;
+	}
+
+	std::cout << "New keypair created!" << std::endl;
+
+	auto report_buffer = GetAttestationReport(enclave_base.get());
+
+	VBS_ENCLAVE_REPORT *report = (VBS_ENCLAVE_REPORT *)((VBS_ENCLAVE_REPORT_PKG_HEADER *)report_buffer.get() + 1);
+
+	/* This is where you would validate the attestation report before using the public key! */
+
+	std::cout << "Beep boop beep, validating attestation report... (for pretend)" << std::endl;
+
+	Sleep(1000);
+
+	std::cout << "Enclave is validated!" << std::endl;
+
+	auto public_key_blob = std::make_unique<uint8_t[]>(sizeof(BCRYPT_ECCKEY_BLOB) + ENCLAVE_REPORT_DATA_LENGTH);
+	BCRYPT_ECCKEY_BLOB *public_key_blob_header = (BCRYPT_ECCKEY_BLOB *)public_key_blob.get();
+
+	public_key_blob_header->dwMagic = BCRYPT_ECDH_PUBLIC_P256_MAGIC;
+	public_key_blob_header->cbKey = ENCLAVE_REPORT_DATA_LENGTH / 2;
+
+	memcpy(public_key_blob_header + 1, report->EnclaveData, ENCLAVE_REPORT_DATA_LENGTH);
+
+	std::cout << "Deriving shared key..." << std::endl;
+
+	auto ephemeral_key = DeriveSharedKey(keypair.get(), public_key_blob.get());
+
+	std::cout << "Successfully derived shared ephemeral key!" << std::endl;
+
+	std::cout << "Encrypting the message:" << std::endl;
+	
+	BCRYPT_AUTHENTICATED_CIPHER_MODE_INFO mode_info{};
+	BCRYPT_INIT_AUTH_MODE_INFO(mode_info);
+	uint8_t tag[16] = {};
+	mode_info.pbTag = tag;
+	mode_info.cbTag = sizeof(tag);
+
+	auto ciphertext_info = EncryptMessage(ephemeral_key.get(), message, mode_info);
+
+	auto ciphertext = std::unique_ptr<uint8_t[]>(ciphertext_info.first);
+
+	size_t bytes_needed = ciphertext_info.second;
+
+	if (ciphertext == nullptr) {
+		return -1;
+	}
+	
+	DecryptDataParams decrypt_data_params = { AllocateCallback, bytes_needed, ciphertext.get(), mode_info.cbTag, mode_info.pbTag};
+	auto decrypted_data = DecryptMessage(enclave_base.get(), decrypt_data_params);
+
 	std::cout << "Data decrypted! Message is:" << std::endl;
-	DumpBytes(decrypt_data_params.decrypted_data, decrypt_data_params.decrypted_size);
+	DumpBytes((uint8_t*)decrypted_data.c_str(), decrypted_data.size() * sizeof(wchar_t));
+	std::cout << std::endl;
 
-	TerminateEnclave(enclave_base, TRUE);
-
-	DeleteEnclave(enclave_base);
+	if (message.compare(decrypted_data) == 0) {
+		std::cout << "The message round-tripped!" << std::endl;
+	}
+	else {
+		std::cerr << "The message did not round-trip!" << std::endl;
+		return -1;
+	}
 
 	return 0;
 }
